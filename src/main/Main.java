8ea73e0853a03b572a63a0fd3cbdefa05f5f0142
Emmanuel Yidana <100804353+Emmanuelitgit@users.noreply.github.com>
2023-07-19 15:10:44 +0000

    individual project

---
+++ src/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+              //STUDENT NAME: Emmanuel Yidana
+              //STUDENT ID: 10868388
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/main.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Inventory Management System!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
